8d76c3ae881e2e78ad4faa06766d6e090caf268a
Golik Vladislav <rozzel@mail.ru>
2022-03-17 11:31:32 +0300

    цикл не нужен - нужна функция

--- js/ads-generation.js
+++ js/ads-generation.js
@@ -17,41 +17,25 @@ const adsGeneration = (serialNumber) => {
   const rawAuthors = createObjectAuthors();
   const rawOffers = createObjectOffers();
   const offers = rawOffers.map((item, index) => ({...item, ...rawAuthors[index]}));
+  const offer = offers[serialNumber - 1];
+  const {title, address, price, type, rooms, guests, checkin, checkout, features, description, photos, avatar} = offer;
   const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
   const mapCanvas = document.querySelector('#map-canvas');
   const cardFragment = document.createDocumentFragment();
-  offers.forEach(({
-    title,
-    address,
-    price,
-    type,
-    rooms,
-    guests,
-    checkin,
-    checkout,
-    features,
-    description,
-    photos,
-    avatar,
-  }) => {
-    const cardElement = cardTemplate.cloneNode(true);
-    const select = (elem) => cardElement.querySelector(elem);
-    const selectAll = (elem) => cardElement.querySelectorAll(elem);
-    offerSelector(select('.popup__title'), 'textContent', title);
-    offerSelector(select('.popup__description'), 'textContent', description);
-    offerSelector(select('.popup__text--address'), 'textContent', address);
-    offerSelector(select('.popup__text--price'), 'innerHTML', price, getOfferPriceTemplate);
-    offerSelector(select('.popup__type'), 'textContent', type, getOfferType);
-    offerSelector(select('.popup__avatar'), 'src', avatar, getOfferType);
-    offerSelector(select('.popup__photos'), 'photos', photos, getOfferPhotos);
-    offerSelector(select('.popup__text--capacity'), ['twoElements', 'innerHTML'], [rooms, guests], getOfferСapacity);
-    offerSelector(select('.popup__text--time'), ['twoElements', 'textContent'], [checkin, checkout], getOfferTime);
-    offerSelector(select('.popup__features'), 'features', features, getOfferFeatures, selectAll('.popup__feature'));
-    cardFragment.appendChild(cardElement);
-  });
-  if (serialNumber) {
-    return mapCanvas.appendChild(cardFragment.children[serialNumber - 1]);
-  }
+  const cardElement = cardTemplate.cloneNode(true);
+  const select = (elem) => cardElement.querySelector(elem);
+  const selectAll = (elem) => cardElement.querySelectorAll(elem);
+  offerSelector(select('.popup__title'), 'textContent', title);
+  offerSelector(select('.popup__description'), 'textContent', description);
+  offerSelector(select('.popup__text--address'), 'textContent', address);
+  offerSelector(select('.popup__text--price'), 'innerHTML', price, getOfferPriceTemplate);
+  offerSelector(select('.popup__type'), 'textContent', type, getOfferType);
+  offerSelector(select('.popup__avatar'), 'src', avatar, getOfferType);
+  offerSelector(select('.popup__photos'), 'photos', photos, getOfferPhotos);
+  offerSelector(select('.popup__text--capacity'), ['twoElements', 'innerHTML'], [rooms, guests], getOfferСapacity);
+  offerSelector(select('.popup__text--time'), ['twoElements', 'textContent'], [checkin, checkout], getOfferTime);
+  offerSelector(select('.popup__features'), 'features', features, getOfferFeatures, selectAll('.popup__feature'));
+  cardFragment.appendChild(cardElement);
   return mapCanvas.appendChild(cardFragment);
 };
 
