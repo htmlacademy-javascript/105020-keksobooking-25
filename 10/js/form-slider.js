5651fc7bc45bd07138c6fb92b2a4201c2d74e00d
Keks <keks@htmlacademy.ru>
2022-04-09 19:38:53 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/form-slider.js
@@ -0,0 +1,41 @@
+const sliderPrice = document.querySelector('.ad-form__slider');
+const valuePrice = document.querySelector('#price');
+
+const createSlider = () => {
+  noUiSlider.create(sliderPrice, {
+    range: {
+      min: 0,
+      max: Number(valuePrice.max),
+    },
+    start: Number(valuePrice.placeholder),
+    step: 1,
+    connect: 'lower',
+    format: {
+      to: function (value) {
+        return value.toFixed(0);
+      },
+      from: function (value) {
+        return parseFloat(value);
+      },
+    },
+  });
+
+  function setValuePrice () {
+    sliderPrice.noUiSlider.set(valuePrice.value);
+  }
+
+  valuePrice.addEventListener('change', setValuePrice);
+};
+
+const sliderPriceUpdate = (callback) => {
+  sliderPrice.noUiSlider.on('update', () => {
+    valuePrice.value = sliderPrice.noUiSlider.get();
+    callback();
+  });
+};
+
+
+export {
+  createSlider,
+  sliderPriceUpdate,
+};
---
+++ 10/js/main.js
@@ -0,0 +1,3 @@
+import './ads-generation.js';
+import './form-accessibility.js';
+import './map.js';
